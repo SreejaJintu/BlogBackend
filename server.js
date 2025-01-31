b5bbad118fc957a7bff55a145747bda9d9f2089c
sreeja o s <sreejaosreenivasan@gmail.com>
2025-01-31 22:11:08 +0530

    deploying

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error(error));
 
+app.get('/', (req, res) => {
+    res.send('Backend running on Render');
+  });
 app.use('/api', postRoutes);
 
 
